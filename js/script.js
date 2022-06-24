4b91bf499fe8db557e4dc95be0a5e0d3f588ddc1
Raul Callejas <84805692+ScorpionCallejas@users.noreply.github.com>
2022-06-24 15:48:11 -0500

    Pequeño cambio

--- js/script.js
+++ js/script.js
@@ -10,12 +10,11 @@ Swal.fire({
 // Formulario de contacto
 let input = document.getElementsByClassName("form_input");
 for (let i = 0; input.length; i++) {
-  input[i].addEventListener("keyup", funcion());
-  function funcion(){
+  input[i].addEventListener("keyup", function() {
     if (this.value.length >= 1) {
       this.nextElementSibling.classList.add("fijar");
     } else {
       this.nextElementSibling.classList.remove("fijar");
     }
-  };
+  });
 }
